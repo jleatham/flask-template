ffd20e2ca50945b97a6ae476b998c44ec2b074b3
jleatham <j.leatham@gmail.com>
2018-12-02 21:20:59 -0600

    form test

--- myflask/myflask.py
+++ myflask/myflask.py
@@ -20,7 +20,7 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Flask Template', user=user, posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -31,6 +31,17 @@ def login():
             form.username.data, form.remember_me.data,testResult))
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/login2', methods=['GET', 'POST'])
+def login2():
+    form = LoginForm()
+    if form.validate_on_submit():
+        testResult = testFunction(form.testField)
+        flash('Login requested for user {}, remember_me={}, testResult={}'.format(
+            form.username.data, form.remember_me.data,testResult))
+        return redirect(url_for('index'))
+    return render_template('login2.html', title='Sign In-2', form=form)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
