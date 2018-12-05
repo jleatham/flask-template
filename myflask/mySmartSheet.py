22bef8901767c21d9923ec8ba80fef8c9ffe3ec0
jleatham <j.leatham@gmail.com>
2018-12-05 10:13:33 -0600

    json error fix

--- myflask/mySmartSheet.py
+++ myflask/mySmartSheet.py
@@ -309,21 +309,21 @@ def ss_update_row(ss_client,archSheet,rowData):
     url = "https://api.smartsheet.com/2.0/sheets/"+str(archSheet)+"/rows"
     headers = {'Authorization': "Bearer "+access_token,'Content-Type': "application/json"}
     payload = '{"toBottom":true, "cells": [ \
-                {"columnId": 6004582892496772, "value": '+ rowData['date'] +', "strict": false}, \
-                {"columnId": 938033311704964, "value": '+ rowData['category'] +', "strict": false}, \
-                {"columnId": 5441632939075460, "value": '+ rowData['bullet'] +', "strict": false}, \
-                {"columnId": 3189833125390212, "value": '+ rowData['bLink'] +', "strict": false}, \
-                {"columnId": 425317295777668, "value": '+ rowData['subBullet1'] +', "strict": false}, \
-                {"columnId": 8588091620386692, "value": '+ rowData['sb1Link'] +', "strict": false}, \
-                {"columnId": 4084491993016196, "value": '+ rowData['subBullet2'] +', "strict": false}, \
-                {"columnId": 6336291806701444, "value": '+ rowData['sb2Link'] +', "strict": false}, \
-                {"columnId": 1832692179330948, "value": '+ rowData['subBullet3'] +', "strict": false}, \
-                {"columnId": 7462191713544068, "value": '+ rowData['sb3Link'] +', "strict": false}, \
-                {"columnId": 2958592086173572, "value": '+ rowData['subBullet4'] +', "strict": false}, \
-                {"columnId": 5210391899858820, "value": '+ rowData['sb4Link'] +', "strict": false}, \
-                {"columnId": 706792272488324, "value": '+ rowData['subBullet5'] +', "strict": false}, \
-                {"columnId": 8025141666965380, "value": '+ rowData['sb5Link'] +', "strict": false}, \
-                {"columnId": 3752783078811524, "value": '+ rowData['arch'] +', "strict": false}, \
+                {"columnId": 6004582892496772, "value": "'+ rowData["date"] +'", "strict": false}, \
+                {"columnId": 938033311704964, "value": "'+ rowData["category"] +'", "strict": false}, \
+                {"columnId": 5441632939075460, "value": "'+ rowData["bullet"] +'", "strict": false}, \
+                {"columnId": 3189833125390212, "value": "'+ rowData["bLink"] +'", "strict": false}, \
+                {"columnId": 425317295777668, "value": "'+ rowData["subBullet1"] +'", "strict": false}, \
+                {"columnId": 8588091620386692, "value": "'+ rowData["sb1Link"] +'", "strict": false}, \
+                {"columnId": 4084491993016196, "value": "'+ rowData["subBullet2"] +'", "strict": false}, \
+                {"columnId": 6336291806701444, "value": "'+ rowData["sb2Link"] +'", "strict": false}, \
+                {"columnId": 1832692179330948, "value": "'+ rowData["subBullet3"] +'", "strict": false}, \
+                {"columnId": 7462191713544068, "value": "'+ rowData["sb3Link"] +'", "strict": false}, \
+                {"columnId": 2958592086173572, "value": "'+ rowData["subBullet4"] +'", "strict": false}, \
+                {"columnId": 5210391899858820, "value": "'+ rowData["sb4Link"] +'", "strict": false}, \
+                {"columnId": 706792272488324, "value": "'+ rowData["subBullet5"] +'", "strict": false}, \
+                {"columnId": 8025141666965380, "value": "'+ rowData["sb5Link"] +'", "strict": false}, \
+                {"columnId": 3752783078811524, "value": "'+ rowData["arch"] +'", "strict": false} \
                 ] }'
     response = requests.request("POST", url, data=payload, headers=headers)
     responseJson = json.loads(response.text)
