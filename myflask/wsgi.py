e5e0e0e1eb5836f1159cbe5a1ee7236eb263f3cc
jleatham <j.leatham@gmail.com>
2018-12-04 21:55:38 -0600

    env debugging

--- myflask/wsgi.py
+++ myflask/wsgi.py
@@ -1,11 +1,6 @@
-#from myflask import app
-
+from myflask import app
 import os
-
-def application(environ, start_response):
-    os.environ['SMARTSHEET_TOKEN'] = environ['SMARTSHEET_TOKEN']
-    from myflask import app as _application
-    return _application(environ, start_response)
+os.environ['SMARTSHEET_TOKEN'] = os.environ.get('SMARTSHEET_TOKEN')
 
 if __name__ == "__main__":
-    _application.run()
+    app.run()
