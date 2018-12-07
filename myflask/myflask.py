14a6fdaec7b6a83676a0604338c69f11eb517868
jleatham <j.leatham@gmail.com>
2018-12-07 14:59:51 -0600

    Update myflask.py

--- myflask/myflask.py
+++ myflask/myflask.py
@@ -109,13 +109,14 @@ def add():
 @app.route('/remove', methods=['POST'])
 def remove():
     ss_client = ss_get_client(access_token)
-
+    addForm = archWeekForm()
+    removeForm = removeArchWeekForm()
     if removeForm.validate_on_submit():
         print('made it to form2 validate')
         print (removeForm.rowID.data)  
     EN_list = ss_get_sheet_parsed(ss_client,archSheet)   
-    addForm = archWeekForm()
-    removeForm = removeArchWeekForm()
+    
+    
     removeForm.rowID.choices = [(x['rowID'],'') for x in EN_list]                 
     return render_template('rendertest2.html', title='Render Test', EN_list=EN_list, addForm=addForm, removeForm=removeForm)
 
