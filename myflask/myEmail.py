eb54cf06828ca04b7c6d7f98d03ed8ce15de0756
jleatham <j.leatham@gmail.com>
2018-12-09 14:51:08 -0600

    Create myEmail.py

---
+++ myflask/myEmail.py
@@ -0,0 +1,21 @@
+from jinja2 import Environment 
+
+def create_email_html(arch):
+    htmlTemplate = """
+        <html>
+        <head>
+        <title>{{ arch }}</title>
+        </head>
+        <body>
+
+        {{ data.hello }}  {{ data.world }}
+
+        </body>
+        </html>
+        """
+    htmlMsg = Environment().from_string(htmlTemplate).render(
+        arch = arch
+        #data = data
+        data = {'hello': 'hello','world':'world'}
+    )
+    print htmlMsg
